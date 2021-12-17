c46ada3bcac08912e5851390b21aa83679a7fbf3
Deepak Kumar <git.logsbydeepak@gmail.com>
2021-12-17 23:42:21 +0530

    add helmet and cookie Parser to server config

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -1,8 +1,12 @@
 import express, { Express, json } from "express";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
 
 import { checkEnv } from "../helper/env.helper";
 
 checkEnv();
 export const server: Express = express();
 
+server.use(helmet());
 server.use(json());
+server.use(cookieParser());
