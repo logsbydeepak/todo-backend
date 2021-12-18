ceb4e0caff0a5dd2bb399ba596d9095458312512
Deepak Kumar <git.logsbydeepak@gmail.com>
2021-12-18 20:23:25 +0530

    hash and salt password before saving to db

--- src/model/schema/user.schema.ts
+++ src/model/schema/user.schema.ts
@@ -1,4 +1,5 @@
 import { Schema } from "mongoose";
+import { hash } from "bcryptjs";
 
 const defaultProperty = {
   required: true,
@@ -9,4 +10,10 @@ export const UserSchema = new Schema({
   name: defaultProperty,
   email: defaultProperty,
   password: defaultProperty,
+});
+
+UserSchema.pre("save", async function (next: any) {
+  if (!this.isModified("password")) return next();
+  this.password = await hash(this.password, 8);
+  return next();
 });
