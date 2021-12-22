75d3b90b92e5941ee2e5eda95c0533693cda8661
Deepak Kumar <git.logsbydeepak@gmail.com>
2021-12-22 16:24:10 +0530

    create logout all router

--- src/router/session.user.ts
+++ src/router/session.user.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { checkAccessToken } from "../middleware/checkAccessToken.middleware";
 
 import { loginSession } from "./session/login.session";
 import { logoutSession } from "./session/logout.session";
 import { refreshSession } from "./session/refresh.session";
+import { checkRefreshToken } from "../middleware/checkRefreshToken.middleware";
+import { checkAccessToken } from "../middleware/checkAccessToken.middleware";
 
 export const sessionRouter = Router();
 
 sessionRouter.get("/", loginSession);
 sessionRouter.delete("/", checkAccessToken, logoutSession);
 sessionRouter.patch("/refresh", refreshSession);
-// userRouter.delete("/refresh");
--- src/router/session/logout.session.ts
+++ src/router/session/logout.session.ts
@@ -8,6 +8,7 @@ import { ErrorResponse, SuccessResponse } from "../../response";
 
 export const logoutSession = async (req: Request, res: Response) => {
   try {
+    const logoutAll = req.query.all;
     const accessToken = req.cookies.accessToken;
     const userId = res.locals.userId;
     const dbToken: any = await TokenModel.findById(userId);
@@ -16,9 +17,13 @@ export const logoutSession = async (req: Request, res: Response) => {
       return ErrorResponse(req, res, "AU", 10);
     }
 
-    dbToken.tokens = dbToken.tokens.filter(
-      (element: any) => !(element.accessToken === accessToken)
-    );
+    if (logoutAll === "true") {
+      dbToken.tokens = [];
+    } else {
+      dbToken.tokens = dbToken.tokens.filter(
+        (element: any) => !(element.accessToken === accessToken)
+      );
+    }
 
     await dbToken.save();
 
