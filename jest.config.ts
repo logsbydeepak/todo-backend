34ade04a0cd8588c17a79116c9983f8881a6deff
Deepak Kumar <git.logsbydeepak@gmail.com>
2021-12-28 17:02:11 +0530

    refactor tsconfig paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,10 @@
+import { compilerOptions } from "./paths.tsconfig.json";
+import { pathsToModuleNameMapper } from "ts-jest";
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  moduleNameMapper: {
-    "@model": ["<rootDir>/src/model"],
-    "@types": ["<rootDir>/src/types"],
-    "@router": ["<rootDir>/src/router"],
-    "@response": ["<rootDir>/src/response"],
-    "@middleware": ["<rootDir>/src/middleware"],
-
-    "@helper/cookie": ["<rootDir>/src/helper/cookie.helper.ts"],
-    "@helper/db": ["<rootDir>/src/helper/db/index.ts"],
-    "@helper/env": ["<rootDir>/src/helper/env.helper.ts"],
-    "@helper/security": ["<rootDir>/src/helper/security.helper.ts"],
-    "@helper/server": ["<rootDir>/src/helper/server.helper.ts"],
-    "@helper/token": ["<rootDir>/src/helper/token.helper.ts"],
-    "@helper/validator": ["<rootDir>/src/helper/validator.helper.ts"],
-
-    "@config/db": ["<rootDir>/src/config/db.config.ts"],
-    "@config/env": ["<rootDir>/src/config/env.config.ts"],
-    "@config/logger": ["<rootDir>/src/config/logger.config.ts"],
-    "@config/server": ["<rootDir>/src/config/server.config.ts"],
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: "<rootDir>/src",
+  }),
 };
